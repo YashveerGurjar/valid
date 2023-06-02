d0bdf9ec5a80bd576a0610a3f74855ee0618c0a9
YashveerGurjar <yash.gurjar@deeporion.com>
2023-06-02 14:41:31 +0530

    validations

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,23 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  def article_params
+    params.require(:article).permit(:name, :age, :phn, :email, :email_confirmation, :termsandcondition)
+  end
+end
---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+    validates :name, presence: { message: ":Please Enter the Name"}
+    validates :age, presence: { message: ":Please Enter the Age"}
+    validates :phn, presence: { message: ":Please Enter the Phn"}, numericality: true , length: { is: 10 }
+    validates :email, confirmation: true
+    validates :termsandcondition, acceptance: true
+
+end
